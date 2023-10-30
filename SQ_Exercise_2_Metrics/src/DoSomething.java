e13b612d7a77b3a260bf7fb0a8d0317549fa0fde
JS-10 <93281725+JS-10@users.noreply.github.com>
2023-10-30 17:52:15 +0100

    Added a few comments to DoSomething.java

--- SQ_Exercise_2_Metrics/src/DoSomething.java
+++ SQ_Exercise_2_Metrics/src/DoSomething.java
@@ -1,15 +1,20 @@
 //Exercise 2 - Task 1 - Java file
 public class DoSomething {
+    //Calculates something. Main focus lies on the code itself and the use of branches, which were required for the previous exercise.
     public int calculation(int a, int b, int c){
+        //Create the sum out of all parameters
         int x = a + b + c;
         while(x < 20){
             if(b < c){
                 if(c < a){
+                    //If c is larger than b and lower than a, then increase value of b by 1.
                     b += 1;
                 }else{
+                    //If c is larger than b, but not lower than a, then increase value of b by 5.
                     b += 5;
                 }
             }else{
+                //If c is not larger than b, then increase the value of x (the sum of all parameters) by 3.
                 x += 3;
             }
         }
